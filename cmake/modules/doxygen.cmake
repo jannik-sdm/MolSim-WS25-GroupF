e7d9b58868123dbbe85b7c034db9dd49c0ca1e1b
Jonah Voigt <jonah.voigt@tum.de>
2025-10-28 14:30:26 +0100

    In doxygen.cmake: This file should now fulfill the requirements: when ENABLE_DOXYGEN_TARGET is activated, make doc_doxygen generates a documentation. Also I'm not totaly sure, if the place, where the documentations are saved is the right one

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,18 @@
 # make doc_doxygen optional if someone does not have / like doxygen
-
 # TODO: create CMake build option for the target.
+option(ENABLE_DOXYGEN_TARGET "Enable doxygen Target" ON)
+if(ENABLE_DOXYGEN_TARGET)
+    find_program(DOXYGEN "doxygen")
+    message(STATUS "doxygen Target is enabled")
+    IF(DOXYGEN)
+        message(STATUS "found doxygen command")
+        add_custom_target(doc_doxygen
+            COMMAND ${DOXYGEN} ..
+            COMMENT "Generate a Doxygen documentation with make doc_doxygen"
+        )
+    else()
+        message(FATAL_ERROR "Could not find doxygen")
+    endif ()
+endif ()
 
 # TODO: Add a custom target for building the documentation.
