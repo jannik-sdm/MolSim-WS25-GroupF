d3481264ef6dfb6ef27f7f799e17751bffc807e9
Jonah Voigt <jonah.voigt@tum.de>
2025-10-29 20:50:27 +0100

    Doxygen is now generated by FindDoxygen in CMake and their in no need for a seperate Doxygen file + README.md is set as the mainpage of the Documentation

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,16 +1,23 @@
 # make doc_doxygen optional if someone does not have / like doxygen
+find_package(Doxygen REQUIRED)
 # TODO: create CMake build option for the target.
 option(ENABLE_DOXYGEN_TARGET "Enable doxygen Target" ON)
 if(ENABLE_DOXYGEN_TARGET)
-    find_program(DOXYGEN "doxygen")
     message(STATUS "doxygen Target is enabled")
-    IF(DOXYGEN)
-        message(STATUS "found doxygen command")
+    find_package(Doxygen REQUIRED)
+
+    IF(DOXYGEN_FOUND)
+        message(STATUS "found doxygen Package")
         # TODO: Add a custom target for building the documentation.
-        add_custom_target(doc_doxygen
-            COMMAND ${DOXYGEN} Doxyfile
-            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-            COMMENT "Generate a Doxygen documentation with make doc_doxygen"
+
+        set(DOXYGEN_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/doxys_documentation)
+        set(DOXYGEN_USE_MDFILE_AS_MAINPAGE README.md)
+
+        doxygen_add_docs(
+                doc_doxygen
+                src/ README.md
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                COMMENT "Generate a Doxygen documentation with make doc_doxygen"
         )
     else()
         message(FATAL_ERROR "Could not find doxygen")
