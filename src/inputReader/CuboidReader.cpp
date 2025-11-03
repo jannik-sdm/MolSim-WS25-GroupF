b7090a9efffa45264354369a86a55689128cdb6e
Raul Bader Zignoli <go69fax@mytum.de>
2025-11-03 23:19:37 +0100

    Add cuboid reader
    Implements a subclass to read files containing cuboid descriptions, one
    per line.

---
+++ src/inputReader/CuboidReader.cpp
@@ -0,0 +1,40 @@
+#include "CuboidReader.h"
+
+#include <array>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "../ParticleGenerator.h"
+
+void CuboidReader::parse(std::vector<Particle> &particles, std::ifstream &file) {
+  Vector3 x;
+  std::array<unsigned int, 3> n;
+  Vector3 v;
+  double distance;
+  double mass;
+
+  std::string current_line;
+
+  if (!file.is_open()) {
+    std::cout << "File no longer open" << std::endl;
+    exit(-1);
+  }
+
+  while (std::getline(file, current_line)) {
+    if (current_line.empty() || current_line[0] == '#') continue;
+
+    std::istringstream data(current_line);
+
+    for (auto &xₖ : x) data >> xₖ;
+    for (auto &nₖ : n) data >> nₖ;
+
+    data >> distance;
+    data >> mass;
+
+    for (auto &vₖ : v) data >> vₖ;
+
+    ParticleGenerator::cuboid(particles, x, n, distance, mass, v);
+  }
+}
