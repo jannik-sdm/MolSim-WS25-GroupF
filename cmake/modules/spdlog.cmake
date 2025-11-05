186c3444bc6151f28c7713b7c3c631edcb1f9442
jv_fedora <joah.voigt@tum.de>
2025-11-05 20:43:28 +0100

    Changes for build

---
+++ cmake/modules/spdlog.cmake
@@ -0,0 +1,17 @@
+find_package(spdlog QUIET)
+
+# Wenn kein Target existiert, FetchContent als Fallback
+if(NOT TARGET spdlog_FOUND)
+    message(STATUS "spdlog not found — using FetchContent...")
+    #https://cmake.org/cmake/help/latest/module/FetchContent.html
+    include(FetchContent)
+    FetchContent_Declare(
+            spdlog
+            GIT_REPOSITORY https://github.com/gabime/spdlog.git
+            GIT_TAG v1.x
+    )
+
+    FetchContent_MakeAvailable(spdlog)
+endif()
+
+target_link_libraries(MolSim PRIVATE spdlog::spdlog)
