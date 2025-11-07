a4aa3dfc74f807a7d555fa06cdede6bf447ee429
go56had <jannik.suendermann@tum.de>
2025-11-07 13:38:11 +0100

    fixed particleContainer not passed as reference

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -13,32 +13,7 @@
 #include "outputWriter/XYZWriter.h"
 #include "utils/ArrayUtils.h"
 
-/**** forward declaration of the calculation functions ****/
 
-/**
- * @brief calculate the force for all particles
- *
- * For each pair of disjunct particles this function calculates the force f with the Formula: \f$ F_{ij} =
- * \frac{m_im_j}{(||x_i-x_j||_2)^3}(x_j-x_i)\f$.
- * Then this function sums up all forces for one particle to calculate the effective force of each particle
- */
-void calculateF();
-
-/**
- * @brief calculate the position for all particles
- *
- * For each particle i this function calculates the position x: \f$ x_i(t_{n+1}) = x_i(t_n)+\Delta t \cdot v_i(t_n) +
- * (\Delta t)^2 \frac{F_i(t_n)}{2m_i}\f$
- */
-void calculateX();
-
-/**
- * @brief calculate the Velocity for all particles
- *
- * For each particle i this function calculates the Velocity v: \f$ v_i(t_{n+1}) = v_i(t_n)+\Delta t
- * \frac{F_i(t_n)+F_i(t_{n+1})}{2m_i}\f$
- */
-void calculateV();
 
 /**
  * @brief plot the particles to a xyz-file or to a vtk-file.
