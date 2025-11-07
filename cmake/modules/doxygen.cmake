bb1bccea0b692c5e06ba8c811f2731619951a355
go56had <jannik.suendermann@tum.de>
2025-11-07 16:32:17 +0100

    fixed doxygen target

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,4 @@
 # make doc_doxygen optional if someone does not have / like doxygen
-find_package(Doxygen REQUIRED)
 # TODO: create CMake build option for the target.
 option(ENABLE_DOXYGEN_TARGET "Enable doxygen Target" ON)
 if(ENABLE_DOXYGEN_TARGET)
