2da588cd8d3e13314e73e1427f444ec3d9e458ea
Raul Bader Zignoli <go69fax@mytum.de>
2025-11-12 13:39:12 +0100

    Test: Improve comparison in ParticleContainer
    Uses the type field as index to do integer comparisons

--- test/TestParticleContainer.cpp
+++ test/TestParticleContainer.cpp
@@ -3,23 +3,37 @@
 #include "ParticleContainer.h"
 #include "gtest/gtest.h"
 class TestParticleContainer : public ::testing::Test {
-  protected:
+ protected:
   ParticleContainer container;
-  int num_particles = 4;
+  static const unsigned int NUM_PARTICLES = 10;
+
   TestParticleContainer() {
-    for (int i = 0; i < num_particles ; i++) {
-      std::array<double, 3> v = {static_cast<double>(i * 3 % 7), static_cast<double>(i * 5 % 8) /3, static_cast<double>((i + 12) * 3)};
-      std::array<double, 3> x = {static_cast<double>(i * 3 % 7), static_cast<double>(i % 3 + 4) /2, static_cast<double>((i + 81) * 12 % 41) *2.4};
+    for (int i = 0; i < NUM_PARTICLES; i++) {
+      std::array<double, 3> v = {static_cast<double>(i * 3 % 7), static_cast<double>(i * 5 % 8) / 3,
+                                 static_cast<double>((i + 12) * 3)};
+      std::array<double, 3> x = {static_cast<double>(i * 3 % 7), static_cast<double>(i % 3 + 4) / 2,
+                                 static_cast<double>((i + 81) * 12 % 41) * 2.4};
       double m = static_cast<double>(i);
-      container.particles.emplace_back(x, v, m);
+
+      container.particles.emplace_back(x, v, m, i);
     }
   }
 };
 
+/**
+ * @test Linear iteration
+ *
+ * This test case checks if the container can be iterated through linearly in order
+ *
+ */
 TEST_F(TestParticleContainer, IterationWorks) {
   int i = 0;
   for (Particle &particle : container) {
-    EXPECT_EQ(container.particles.at(i).getM(), particle.getM());
-    i++;
+    EXPECT_EQ(particle.getType(), i);
+
+    if (i++ >= NUM_PARTICLES) {
+      FAIL();
+    }
   }
 }
+
