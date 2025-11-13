be0805c2e4737b351398a0dbbf917adc79e52f46
Raul Bader Zignoli <go69fax@mytum.de>
2025-11-13 12:17:23 +0100

    Test: Fix FileReader
    The XYZReader test has wrong indices and is missing the number of particles at the start

--- test/TestFileReader.cpp
+++ test/TestFileReader.cpp
@@ -16,6 +16,7 @@ TEST(Reader, XYZ) {
   const double mass = 4.776e-14;
 
   std::stringstream mockInput;
+  mockInput << "1" << std::endl;
   mockInput << p[0] << " " << p[1] << " " << p[2] << "    ";
   mockInput << v[0] << " " << v[1] << " " << v[2] << "    ";
   mockInput << mass << std::endl;
@@ -23,12 +24,12 @@ TEST(Reader, XYZ) {
   XYZReader::parse(particles, mockInput);
 
   EXPECT_DOUBLE_EQ(p[0], particles[0].getX()[0]);
-  EXPECT_DOUBLE_EQ(p[1], particles[1].getX()[1]);
-  EXPECT_DOUBLE_EQ(p[2], particles[2].getX()[2]);
+  EXPECT_DOUBLE_EQ(p[1], particles[0].getX()[1]);
+  EXPECT_DOUBLE_EQ(p[2], particles[0].getX()[2]);
 
   EXPECT_DOUBLE_EQ(v[0], particles[0].getV()[0]);
-  EXPECT_DOUBLE_EQ(v[1], particles[1].getV()[1]);
-  EXPECT_DOUBLE_EQ(v[2], particles[2].getV()[2]);
+  EXPECT_DOUBLE_EQ(v[1], particles[0].getV()[1]);
+  EXPECT_DOUBLE_EQ(v[2], particles[0].getV()[2]);
 
   EXPECT_DOUBLE_EQ(mass, particles[0].getM());
 }
