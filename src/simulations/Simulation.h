5accb437719885a869803c5e5ae218553dab3ed4
go56had <jannik.suendermann@tum.de>
2025-11-13 19:14:46 +0100

    added documentation for the iteration method of Simulation

--- src/simulations/Simulation.h
+++ src/simulations/Simulation.h
@@ -9,6 +9,13 @@ public:
   // desctructor to avoid memory leaks
   virtual ~Simulation() = default;
 
+  /**
+  * @brief Performs one complete iteration of the simulation.
+ *
+ * This is a virtual function defining the core simulation step
+ * for all concrete simulation types. Derived classes must implement
+ * this method to execute one complete cycle of the respective simulation logic.
+   */
   virtual void iteration() = 0;
 
 };
