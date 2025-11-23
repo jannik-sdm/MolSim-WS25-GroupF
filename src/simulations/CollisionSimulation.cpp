8ae3a971cf1c08f8c2867d448cea825a469644f5
Raul Bader Zignoli <go69fax@mytum.de>
2025-11-23 09:08:52 +0100

    Move Formulas out to different file
    Allows for future unit tests and better reusability

--- src/simulations/CollisionSimulation.cpp
+++ src/simulations/CollisionSimulation.cpp
@@ -3,27 +3,26 @@
 //
 
 #include "CollisionSimulation.h"
+
 #include "../utils/ArrayUtils.h"
 #include "../utils/MaxwellBoltzmannDistribution.h"
+#include "Physics.h"
 
-CollisionSimulation::CollisionSimulation(ParticleContainer &container, const double end_time, const double delta_t) : PlanetSimulation(container, end_time, delta_t) {
+CollisionSimulation::CollisionSimulation(ParticleContainer &container, const double end_time, const double delta_t)
+    : PlanetSimulation(container, end_time, delta_t) {
   // initialize particles with brownian motion
   for (auto &p : particleContainer) {
     Vector3 v = maxwellBoltzmannDistributedVelocity(brownian_motion_avg_velocity, 2);
     p.setV(p.getV() + v);
   }
-
 }
+
 void CollisionSimulation::calculateF() {
   for (auto &p : particleContainer) p.setF({0, 0, 0});
   for (auto [p1, p2] : particleContainer.pairs()) {
-    // force calculation
-    const double norm = ArrayUtils::L2Norm(p1.getX() - p2.getX());
-    const double a = pow(sigma / norm, 6) - 2 * pow(sigma / norm, 12);
-    const double b = -(24*epsilon) / pow(norm, 2);
-    const Vector3 force = a*b*(p1.getX()- p2.getX());
+    Vector3 f = Physics::lennardJonesForce(p1, p2, sigma, epsilon);
 
-    p1.addF(force);
-    p2.subF(force);
+    p1.addF(f);
+    p2.subF(f);
   }
 }
--- src/simulations/PlanetSimulation.h
+++ src/simulations/PlanetSimulation.h
@@ -3,46 +3,42 @@
 //
 
 #pragma once
+
 #include "../ParticleContainer.h"
 #include "Simulation.h"
 
 class PlanetSimulation : public Simulation {
-public:
-
+ public:
   PlanetSimulation(ParticleContainer &container, double end_time, double delta_t);
   /**
-    * Calculates one timestep of the simulation and applies the changes to the particles.
-    */
+   * Calculates one timestep of the simulation and applies the changes to the particles.
+   */
   void iteration() override;
 
-protected:
+ protected:
   ParticleContainer &particleContainer;
   const double end_time;
   const double delta_t;
 
   /**
- * @brief calculate the force for all particles
- *
- * For each pair of disjunct particles this function calculates the force f with the Formula: \f$ F_{ij} =
- * \frac{m_im_j}{(||x_i-x_j||_2)^3}(x_j-x_i)\f$.
- * Then this function sums up all forces for one particle to calculate the effective force of each particle
- */
+   * @brief calculate the force for all particles
+   *
+   * For each pair of disjunct particles this function calculates the force between the two particles.
+   * Then this function sums up all forces for one particle to calculate the effective force of each particle
+   */
   virtual void calculateF();
 
   /**
    * @brief calculate the position for all particles
    *
-   * For each particle i this function calculates the position x: \f$ x_i(t_{n+1}) = x_i(t_n)+\Delta t \cdot v_i(t_n) +
-   * (\Delta t)^2 \frac{F_i(t_n)}{2m_i}\f$
+   * For each particle i this function calculates the new position x.
    */
   virtual void calculateX();
 
   /**
    * @brief calculate the Velocity for all particles
    *
-   * For each particle i this function calculates the Velocity v: \f$ v_i(t_{n+1}) = v_i(t_n)+\Delta t
-   * \frac{F_i(t_n)+F_i(t_{n+1})}{2m_i}\f$
+   * For each particle i this function calculates the new Velocity v
    */
   virtual void calculateV();
 };
-
