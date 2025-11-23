cee2c87efab34e03725047e5e34cb95f15fd0b4b
Raul Bader Zignoli <go69fax@mytum.de>
2025-11-23 09:35:38 +0100

    Test: Create Header for TestParticleContainer
    Moves the Test class into its own header file

--- test/TestParticleContainer.cpp
+++ test/TestParticleContainer.cpp
@@ -1,24 +1,18 @@
 // https://google.github.io/googletest/primer.html
 
-#include "ParticleContainer.h"
-#include "gtest/gtest.h"
-class TestParticleContainer : public ::testing::Test {
- protected:
-  ParticleContainer container;
-  static const unsigned int NUM_PARTICLES = 10;
+#include "TestParticleContainer.h"
 
-  TestParticleContainer() {
-    for (int i = 0; i < NUM_PARTICLES; i++) {
-      std::array<double, 3> v = {static_cast<double>(i * 3 % 7), static_cast<double>(i * 5 % 8) / 3,
-                                 static_cast<double>((i + 12) * 3)};
-      std::array<double, 3> x = {static_cast<double>(i * 3 % 7), static_cast<double>(i % 3 + 4) / 2,
-                                 static_cast<double>((i + 81) * 12 % 41) * 2.4};
-      double m = static_cast<double>(i);
+TestParticleContainer::TestParticleContainer() {
+  for (int i = 0; i < NUM_PARTICLES; i++) {
+    std::array<double, 3> v = {static_cast<double>(i * 3 % 7), static_cast<double>(i * 5 % 8) / 3,
+                               static_cast<double>((i + 12) * 3)};
+    std::array<double, 3> x = {static_cast<double>(i * 3 % 7), static_cast<double>(i % 3 + 4) / 2,
+                               static_cast<double>((i + 81) * 12 % 41) * 2.4};
+    double m = static_cast<double>(i);
 
-      container.particles.emplace_back(x, v, m, i);
-    }
+    container.particles.emplace_back(x, v, m, i);
   }
-};
+}
 
 /**
  * @test Linear iteration
