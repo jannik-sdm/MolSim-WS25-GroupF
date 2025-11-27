f0ddfe3fbbe139f3f46821195402617f5b20e866
go56had <jannik.suendermann@tum.de>
2025-11-27 19:19:34 +0100

    structure of tests (no context because of cmake)

---
+++ test/TestLinkedCells.h
@@ -0,0 +1,22 @@
+//
+// Created by jannik on 11/26/25.
+//
+/*
+#pragma once
+
+#include <vector>
+
+#include "LinkedCells/LinkedCells.h"
+#include "Particle.h"
+#include <gtest/gtest.h>
+
+
+class TestLinkedCells : public ::testing::Test {
+protected:
+  LinkedCells linkedCells;
+  std::vector<Particle> particles;
+
+public:
+  TestLinkedCells();
+};
+*/
