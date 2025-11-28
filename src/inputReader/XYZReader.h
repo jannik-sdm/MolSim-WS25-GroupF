14d2e9d311280b8494137518c04a805ae4345eac
Raul Bader Zignoli <go69fax@mytum.de>
2025-11-28 15:27:52 +0100

    Documentation: Document Input readers
    Adds more documentation to input file readers

--- src/inputReader/XYZReader.h
+++ src/inputReader/XYZReader.h
@@ -20,5 +20,11 @@
  */
 class XYZReader : public FileReader<XYZReader> {
  public:
+  /**
+   * @brief Parses a stream of the `file` in xyz format into `particles`
+   *
+   * @param particles Where to store the read particles
+   * @param file Filestream to read from
+   */
   static void parse(std::vector<Particle> &particles, std::istream &file);
 };
