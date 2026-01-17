561a740a345713906683e1851191e0adc675d3fa
Raul Bader Zignoli <go69fax@mytum.de>
2026-01-17 18:05:13 +0100

    Cmake: Add include directory for OpenMP
    Another fix for clang-tidy

--- cmake/modules/openmp.cmake
+++ cmake/modules/openmp.cmake
@@ -6,6 +6,7 @@ if (ENABLE_OPENMP)
 	if (OpenMP_FOUND)
 		message(STATUS "OpenMP found, enabling support")
 
+		target_include_directories(MolSimLib PUBLIC ${OpenMP_CXX_INCLUDE_DIRS})
 		target_link_libraries(MolSimLib OpenMP::OpenMP_CXX)
 	else()
 		message(FATAL_ERROR "OpenMP not found")
