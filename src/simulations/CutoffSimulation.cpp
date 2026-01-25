610334a9b89ed320dab6b7e58a62a51834a549da
Jonah Voigt <joah.voigt@tum.de>
2026-01-25 22:00:32 +0100

    Deactivated trace and Debug logs

--- src/simulations/CutoffSimulation.cpp
+++ src/simulations/CutoffSimulation.cpp
@@ -11,11 +11,11 @@
 #include "utils/MaxwellBoltzmannDistribution.h"
 
 void CutoffSimulation::iteration() {
-  spdlog::debug("Updating Positions");
+  //spdlog::debug("Updating Positions");
   updateX();
-  spdlog::debug("Updating Forces");
+  //spdlog::debug("Updating Forces");
   updateF();
-  spdlog::debug("Updating Velocities");
+  //spdlog::debug("Updating Velocities");
   updateV();
 }
 
@@ -39,10 +39,10 @@ void CutoffSimulation::updateF() {
 void CutoffSimulation::updateX() {
   linkedCells.applyToParticles([this](Particle &p) {
     if (p.getState() < 0) return;
-    spdlog::trace("Updating X:");
-    spdlog::trace("-> Old Position: ({},{},{})", p.getX()[0], p.getX()[1], p.getX()[2]);
+    //spdlog::trace("Updating X:");
+    //spdlog::trace("-> Old Position: ({},{},{})", p.getX()[0], p.getX()[1], p.getX()[2]);
     p.setX(Physics::StoermerVerlet::position(p, delta_t));
-    spdlog::trace("-> New: ({},{},{})", p.getX()[0], p.getX()[1], p.getX()[2]);
+    //spdlog::trace("-> New: ({},{},{})", p.getX()[0], p.getX()[1], p.getX()[2]);
   });
   linkedCells.moveParticles();
 }
@@ -50,10 +50,10 @@ void CutoffSimulation::updateX() {
 void CutoffSimulation::updateV() {
   linkedCells.applyToParticles([this](Particle &p) {
     if (p.getState() < 0) return;
-    spdlog::trace("Updating V:");
-    spdlog::trace("-> Old Velocity: ({},{},{})", p.getV()[0], p.getV()[1], p.getV()[2]);
+    //spdlog::trace("Updating V:");
+    //spdlog::trace("-> Old Velocity: ({},{},{})", p.getV()[0], p.getV()[1], p.getV()[2]);
     p.setV(Physics::StoermerVerlet::velocity(p, delta_t));
-    spdlog::trace("-> New Velocity: ({},{},{})", p.getV()[0], p.getV()[1], p.getV()[2]);
+    //spdlog::trace("-> New Velocity: ({},{},{})", p.getV()[0], p.getV()[1], p.getV()[2]);
   });
 }
 
