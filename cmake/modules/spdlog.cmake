f700f7fa236bd1bbce05c6b5eaf841300ea7cfa5
Jonah Voigt <jonah.voigt@tum.de>
2026-01-27 15:34:12 +0100

    Ich habe mal einen neuen branch gemacht, weil ich das Gefühl habe, dass es mit macro SPD logs langsamer ist und ich mir den branch nicht kaputt machen möchte

--- cmake/modules/spdlog.cmake
+++ cmake/modules/spdlog.cmake
@@ -12,6 +12,48 @@ if(NOT TARGET spdlog::spdlog)
     )
 
     FetchContent_MakeAvailable(spdlog)
+    
 endif()
 
 target_link_libraries(MolSimLib spdlog::spdlog)
+
+set(LOG_LEVEL "INFO"
+    CACHE STRING "Set spdlog log level"
+)
+
+set_property(CACHE LOG_LEVEL PROPERTY STRINGS
+    TRACE DEBUG INFO WARN ERROR OFF
+)
+
+
+if(LOG_LEVEL STREQUAL "TRACE")
+    set(SPDLOG_LEVEL SPDLOG_LEVEL_TRACE)
+elseif(LOG_LEVEL STREQUAL "DEBUG")
+    set(SPDLOG_LEVEL SPDLOG_LEVEL_DEBUG)
+elseif(LOG_LEVEL STREQUAL "INFO")
+    set(SPDLOG_LEVEL SPDLOG_LEVEL_INFO)
+elseif(LOG_LEVEL STREQUAL "WARN")
+    set(SPDLOG_LEVEL SPDLOG_LEVEL_WARN)
+elseif(LOG_LEVEL STREQUAL "ERROR")
+    set(SPDLOG_LEVEL SPDLOG_LEVEL_ERROR)
+elseif(LOG_LEVEL STREQUAL "OFF")
+    set(SPDLOG_LEVEL SPDLOG_LEVEL_OFF)
+else()
+    message(FATAL_ERROR "Invalid LOG_LEVEL '${LOG_LEVEL}'. Must be one of: DEBUG, INFO, WARN, ERROR, OFF")
+endif()
+
+if(TARGET MolSimLib)
+    target_compile_definitions(MolSimLib
+        PRIVATE SPDLOG_ACTIVE_LEVEL=${SPDLOG_LEVEL}
+    )
+endif()
+if(TARGET MolSim)
+    target_compile_definitions(MolSim
+        PRIVATE SPDLOG_ACTIVE_LEVEL=${SPDLOG_LEVEL}
+    )
+endif()
+if(TARGET MolSimTester)
+    target_compile_definitions(MolSimTester
+        PRIVATE SPDLOG_ACTIVE_LEVEL=${SPDLOG_LEVEL}
+    )
+endif()
